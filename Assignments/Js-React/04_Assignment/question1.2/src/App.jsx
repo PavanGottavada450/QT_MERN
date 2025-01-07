c96c7cec321e6e1b61111ebe2830dc298df09257
ramu <gottavadapavan@gmail.com>
2025-01-07 23:34:26 +0530

    files added

--- Assignments/Js-React/04_Assignment/question1.2/src/App.jsx
+++ Assignments/Js-React/04_Assignment/question1.2/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import PdfViewer from "./Components/PdfViewer";
 
 
 function App() {
 
   return (
     <div>
-
+      <PdfViewer />
     </div>
   );
 }
---
+++ Assignments/Js-React/04_Assignment/question1.2/src/Components/PdfViewer.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+function PdfViewer() {
+  return (
+    <div>
+        <form >
+            <input type="text" />
+            <button>View PDF</button>
+        </form>
+        <div className="pdf-container">
+             
+        </div>
+    </div>
+  )
+}
+
+export default PdfViewer
