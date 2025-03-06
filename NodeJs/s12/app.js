24845b0b63e8108582bc025982347a246ab35cab
ramu <gottavadapavan@gmail.com>
2025-03-06 22:30:04 +0530

    files added

--- NodeJs/s12/app.js
+++ NodeJs/s12/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const fs = require("fs")
 
 app.get("/text",(req,res)=>{
     res.send("My First API using Express : ...");
 }); //   http://localhost:4321/text
+
+
+app.get("/home",(req,res)=>{
+    fs.readFile("home.html","utf-8",(error,data)=>{
+        if(error){
+            res.send("<h1>Failed to read content of the file</h1>")
+        } else{
+            res.send(data)
+        }
+    })
+});
 
 app.listen(4321,()=>{
     console.log("server started on port number 4321");
---
+++ NodeJs/s13/cilent/signup.js
@@ -0,0 +1,9 @@
+
+const email = document.getElementById("email").value;
+const phone = document.getElementById("phone").value;
+
+function fnuser(){
+    const name = document.getElementById("name").value;
+    console.log(name);
+    
+}
