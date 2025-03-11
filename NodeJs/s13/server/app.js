e70b0a415ed9648587fd28dd2f1999c065be2786
ramu <gottavadapavan@gmail.com>
2025-03-11 23:12:01 +0530

    files added

--- NodeJs/s13/server/app.js
+++ NodeJs/s13/server/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+
 const port = 8978;
 const app = express();
 
@@ -8,10 +10,27 @@ app.use(express.json());
 
 app.post("/user", (req, res) => {
   const user = req.body;
-  console.log(user);
-  res.send({
-    ok: true,
-    result: "Working",
+
+  fs.readFile("data.json", "utf-8", (error, filedata) => {
+    if (error) {
+      res.send({
+        ok: false,
+        result: "Failed to access the data from the file",
+      });
+    } else {
+      let data = JSON.parse(filedata);
+      data.data.push(user);
+      fs.writeFile("data.json", JSON.stringify(data), (error) => {
+        if (error) {
+          res.send({
+            ok: false,
+            result: "Failed to Register the User",
+          });
+        } else {
+          res.send({ ok: true, result: "User Registered Successfully" });
+        }
+      });
+    }
   });
 });
 
